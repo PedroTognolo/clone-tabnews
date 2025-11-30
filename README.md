283649c78355e6add4c09e39d9c28fbeac6546c2
pedrotognolo <pedrotognolo@gmail.com>
2025-11-30 18:09:59 -0300

    configurando o

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 clone de https://www.tabnews.com.br para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 function Home() {
-    return <div>
-            <h1>Neneim!</h1>
-            <h2>vc é sucesso! tudo de bom! speek english, corre rápido, é mto inteligente e maravilhosa! love you!</h2>
-        </div>
+  return (
+    <div>
+      <h1>Neneim!</h1>
+      <h2>
+        vc é sucesso! tudo de bom! speek english, corre rápido, é mto
+        inteligente e maravilhosa! love you!
+      </h2>
+    </div>
+  );
 }
 
 export default Home;
